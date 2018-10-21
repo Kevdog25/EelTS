48c5fd824e40839dd5e32f89182be6008e32660c
Kevin Welsh <kevinwelsh132@gmail.com>
2018-10-21 14:26:19 -0500

    Is now a package

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='eel',
+      version='0.1',
+      description='',
+      url='http://github.com/Kevdog25/Eel',
+      author='Kevin Welsh',
+      license='MIT',
+      packages=['eel'],
+      zip_safe=False)
